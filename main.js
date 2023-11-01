4052e2047f9252d0e6e51a560c55a3af8b7d5d79
Saad Niazi <saadniazifed+1@gmail.com>
2023-11-01 10:10:55 +0500

    Simple check using JS

---
+++ main.js
@@ -0,0 +1,9 @@
+const email = document.querySelector("#mail");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
