35fed1f8aed2fd2304ecabc2fff80e3330615eb8
Saad Niazi <saadniazifed+1@gmail.com>
2023-11-01 21:06:52 +0500

    Checking basic validation using JS

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-const email = document.querySelector("#mail");
+// There are many ways to pick a DOM node; here we get the form itself and the email
+// input box, as well as the span element into which we will place the error message.
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
 
 email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an email address!");
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
   } else {
-    email.setCustomValidity("");
+    showError();
   }
 });
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address!";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value must be an email address";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters`;
+  }
+  emailError.className = "error active";
+}
